c3611a5604fb99d874aa587c3768b198fb9e48ec
nik <nik@localhost.localdomain>
2020-03-05 13:57:51 -0500

    complit_task_rm_dir_static

--- test_lab/login/views.py
+++ test_lab/login/views.py
@@ -1,9 +1,22 @@
+#from django.core.context_processors import csrf
+#from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpResponse
 
 
 # Create your views here.
 class LoginView(View):
-
+    
+    #@csrf_exempt
     def get(self, request):
         return render(request, 'login/index.html')
+
+    #@csrf_exempt
+    def post(self, request):
+        html = '<html><body>'
+        for k, v in request.POST.items():
+            html += f'{k}: {v}<br>'
+        html += '</body></html>'
+        return HttpResponse(html)
+
